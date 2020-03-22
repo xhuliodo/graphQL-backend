b747f0a7281b650d406d93a02bf8597400f32dab
xhuliodo <xhuliodo@gmail.com>
2020-03-22 21:18:13 +0100

    adding config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGODB: 'mongodb+srv://graph-ql:graph-ql@always-learning-ytq1g.mongodb.net/merng?retryWrites=true&w=majority',
+    TOKENSECRET: 'In the night I hear them talk, Coldest story ever told'
+
+}
